dbe4890bb58f801aacc55acc54e3cfa3efb4281a
sotoflore <soto_flores98@hotmail.com>
2021-11-04 16:31:42 -0500

    Add-main

---
+++ java-script.js
@@ -0,0 +1,19 @@
+let typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">Sin embargo, aunque no podemos adivinar el mundo que será,bien podemos imaginar el que queremos que sea. El derecho de soñar no figura entre los treinta derechos humanos que las Naciones Unidas proclamaron a fines de 1948.Pero si no fuera por él, y por las aguas que da de beber, los demás derechos se morirían de sed. <br><br><br>Eduardo Galeano.</i>',
+		'<i class="mascota">La educación es algo admirable,pero es bueno recordar de cuando en cuando que nada de lo que merece <br>la pena saber puede ser enseñado.<br><br><br>Oscar Wilde.</i>',
+		'<i class="mascota">Cuando es verdadera, cuando nace de la necesidad de decir,a la voz humana no hay quien la pare.<br>Si le niegan la boca, ella habla por las manos,o por los ojos, o por los poros, o por donde sea.<br><br><br> Eduardo Galeano.</i>',
+		'<i class="mascota">Nunca pensé que en la felicidad hubiera tanta tristeza.<br><br><br>Mario Benedetti.</i>',
+        '<i class="mascota">Las cosas simples son las más extraordinarias y sólo los sabios consiguen verlas.<br><br><br>Paulo Coelho.</i>',
+
+	],
+	stringsElement: '#cadenas-texto',
+	typeSpeed: 75,
+	startDelay: 300, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1500, 
+	loop: true, 
+	loopCount: false, 
+	contentType: 'html',
+});
